9feea9908b2c0973afb3abf1e6be242083c42366
rares221 <58572073+rares221@users.noreply.github.com>
2020-10-24 21:13:14 +0300

    Finished the last task.

--- WPF_Framework/Interfata2014/ISIP_Framework/ISIP_Algorithms/Tools/Tools.cs
+++ WPF_Framework/Interfata2014/ISIP_Framework/ISIP_Algorithms/Tools/Tools.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Windows.Interop;
 using Emgu.CV;
 using Emgu.CV.Structure;
 namespace ISIP_Algorithms.Tools
@@ -21,20 +21,36 @@ namespace ISIP_Algorithms.Tools
             }
             return Result;
         }
+        public static List<byte> GammaLUT(double gamma)
+        {
+            double a = 255 / Math.Pow(255, gamma);
+            List<byte> LUT = new List<byte>();
+            for(int i=0;i<256;i++)
+            {
+                double f = (byte)(a * Math.Pow(i, gamma));
+                LUT.Add((byte)f);
+            }
+            return LUT;
+        }
         public static Image<Gray, byte> GammaCorrection(Image<Gray, byte> InputImage, double gamma)
         {
             Image<Gray, byte> Result = new Image<Gray, byte>(InputImage.Size);
-            double a = 255 / Math.Pow(255, gamma);
+            List<byte> Lookup = GammaLUT(gamma);
 
             for (int y = 0; y < InputImage.Height; y++)
             {
                 for (int x = 0; x < InputImage.Width; x++)
                 {
-                    Result.Data[y, x, 0] = (byte)(a * Math.Pow(InputImage.Data[y, x, 0], gamma));
+                    Result.Data[y, x, 0] = (byte)(Lookup.ElementAt (InputImage.Data[y, x, 0]));
                 }
             }
             return Result;
         }
+
+
+
+
+
 
 
     }
